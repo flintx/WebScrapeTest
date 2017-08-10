1855986dcb9de19793cde6483d02860662f28a10
flintx <whuflint@foxmail.com>
2017-08-10 19:41:41 +0800

    新增单元测试.

--- selenium_learn.py
+++ selenium_learn.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class PythonOrgSearch(unittest.TestCase):
-
+class SeleniumTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
+    @unittest.skip("don't need.")
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("http://www.python.org")
@@ -16,7 +16,23 @@ class PythonOrgSearch(unittest.TestCase):
         elem = driver.find_element_by_name("q")
         elem.send_keys("pycon")
         elem.send_keys(Keys.RETURN)
+        # print(driver.page_source)
         assert "No results found." not in driver.page_source
+
+    def test_login_in_bilibili_com(self):
+        driver = self.driver
+        driver.get("https://passport.bilibili.com/login")
+        self.assertIn("bilibili", driver.title)
+        elem_username = driver.find_element_by_id("login-username")
+        elem_username.send_keys("example@mail.com")
+        elem_password = driver.find_element_by_id("login-passwd")
+        elem_password.send_keys("example_password")
+        driver.save_screenshot("E:\\screen.png")
+        # print(driver.page_source)
+
+    def tearDown(self):
+        self.driver.close()
+
 
 if __name__ == '__main__':
     # browser = webdriver.Chrome()
