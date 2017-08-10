31ff15d5dcfe72b890cf4e406caa5f49af085797
flintx <whuflint@foxmail.com>
2017-08-10 17:45:12 +0800

    no message.

---
+++ chapter1/useragent.py
@@ -0,0 +1,21 @@
+from urllib.error import URLError
+from urllib.request import urlopen
+
+
+def download(url, num_retries=2):
+    print("Downloading: " + url)
+    try:
+        html = urlopen(url).read()
+    except URLError as e:
+        print("Error: " + e.reason)
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                download(url, num_retries - 1)
+    return html
+
+
+if __name__ == '__main__':
+    my_url = "http://example.webscraping.com/"
+    res = download(my_url)
+    print(res)
